289ba6f96f251ae00645ac9e7802fe759b6228f0
terminatedNULL <alexander.yauchler@gmail.com>
2023-09-13 13:46:05 -0400

    Implemented resizeImage()

--- src/main/kotlin/utils/imageTools.kt
+++ src/main/kotlin/utils/imageTools.kt
@@ -1,2 +1,9 @@
 package utils
 
+import java.awt.image.BufferedImage;
+
+fun resizeImage(img: BufferedImage, tWidth: Int, tHeight: Int) : BufferedImage {
+    val resizedImg = BufferedImage(tWidth, tHeight, BufferedImage.TYPE_INT_ARGB)
+    resizedImg.createGraphics().drawImage(img, 0, 0, tWidth, tHeight, null)
+    return resizedImg
+}
--- src/main/kotlin/utils/sprite.kt
+++ src/main/kotlin/utils/sprite.kt
@@ -1,23 +1,36 @@
 package utils
 
-import java.awt.Graphics
+import java.awt.Color
+import java.awt.Graphics2D
+import java.awt.GraphicsConfigTemplate
+import java.awt.GraphicsConfiguration
+import java.awt.GraphicsEnvironment
 import java.awt.Image
+import java.awt.Transparency
 import java.awt.image.BufferedImage
 import java.awt.image.ImageObserver
 
-public class Sprite(
-    var x: Int,
-    var y: Int,
-    var width: Int,
-    var height: Int
-) : drawObject, physicsObject {
+public class Sprite(x: Int, y: Int, width: Int, height: Int) : drawObject, physicsObject {
+    public var x = x
+    public var y = y
+    var width = width
+    var height = height
     override val uuid: Int = register()
     lateinit var texture: BufferedImage
 
-    fun setImage(img: BufferedImage) { texture = img }
-    fun getImage() : BufferedImage { return texture }
+    fun setImage(img: BufferedImage) {
+        val gConfig: GraphicsConfiguration =
+            GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice.defaultConfiguration
+        val gpuImage: Image = gConfig.createCompatibleImage(img.getWidth(null), img.getHeight(null), Transparency.BITMASK)
+        texture = img
+    }
+    fun getImage() : Image { return texture }
 
-    override fun draw(gpx: Graphics) {
+    override fun draw(wind: epWindow) {
+        val gpx: Graphics2D = wind.bufStrat.drawGraphics as Graphics2D
+        gpx.drawImage(wind.backImg, 0, 0, null)
         gpx.drawImage(texture, x, y, null)
+        gpx.dispose()
+        wind.bufStrat.show()
     }
 }
