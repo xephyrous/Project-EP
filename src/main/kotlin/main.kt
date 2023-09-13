695dad328aab9bc418d2ef7de14b260b1dcba31a
terminatedNULL <alexander.yauchler@gmail.com>
2023-09-13 16:23:02 -0400

    Added test comments

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,13 +9,16 @@ fun main() {
     //Create main window
     val mainWind = epWindow("Project EP", 800, 400)
 
+    /* Base code to load a sprite
     val imgF = File("src/main/Assets/motherboard.png")
     val img: BufferedImage = ImageIO.read(imgF)
     val testSprite: Sprite = Sprite(0, 0, img.width, img.height)
     testSprite.setImage(img)
+     */
 
     while(true) {
+        /* Base code to draw  a sprite
         testSprite.draw(mainWind)
-        testSprite.x++
+         */
     }
 }
