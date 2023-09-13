4da062bb261186c07aa9005dcca474cb9a7b31f2
terminatedNULL <alexander.yauchler@gmail.com>
2023-09-13 02:35:14 -0400

    Main image tests & imageTools file created

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,20 @@
-import java.awt.*
-import javax.swing.*
-
-import utils.*
+import utils.Sprite
+import utils.epWindow
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
 
 fun main() {
+    //Create main window
+    val mainWind = epWindow("Project EP", 800, 400)
+    mainWind.isVisible = true
 
+
+    val imgF = File("src/main/Assets/motherboard.png")
+    val img: BufferedImage = ImageIO.read(imgF)
+    val testSprite: Sprite = Sprite(0, 0, img.width, img.height)
+
+    testSprite.setImage(img)
+    while(true)
+        testSprite.draw(mainWind.getGraphics())
 }
