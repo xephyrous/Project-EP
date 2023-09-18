e041a8297fbf3b7849895094e4b2f2b6cfccdfd1
terminatedNULL <alexander.yauchler@gmail.com>
2023-09-18 16:44:29 -0400

    Updated main image test, updated base image

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -18,14 +18,19 @@ fun main() {
     val screenSize: Dimension = Toolkit.getDefaultToolkit().screenSize
     val mainWind = epWindow("Project EP", screenSize.width, screenSize.height)
 
+
+    println(screenSize)
+
     // Base code to load a sprite and draw to the main window
     /*
     val imgF = File("src/main/Assets/project_ep_logo.png")
-    val img: BufferedImage = ImageIO.read(imgF)
+    var img: BufferedImage = ImageIO.read(imgF)
+    img = resizeImage(img, img.width / 2, img.height / 2)
     val testSprite: Sprite = Sprite(0.0, 0.0, img.width, img.height)
     testSprite.setImage(img)
     mainWind.addObject(testSprite)
-     */
+    */
+
 
     while(running) {
         val delta: Long = System.currentTimeMillis() - lastLoopTime
